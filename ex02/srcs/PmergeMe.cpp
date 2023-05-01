edc18fc4ff8da8e774f31797a78d8686de1dd251
dracken24 <dracken24@gmail.com>
2023-04-30 22:14:07 -0400

    presque en ordre

--- ex02/srcs/PmergeMe.cpp
+++ ex02/srcs/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: dracken24 <dracken24@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 14:15:18 by dracken24         #+#    #+#             */
-/*   Updated: 2023/04/30 21:01:32 by dracken24        ###   ########.fr       */
+/*   Updated: 2023/04/30 22:13:02 by dracken24        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,68 +86,99 @@ static bool	IsDigit(std::string str)
 // Sort numbers from lesser to highter
 std::vector<uint32> PmergeMe::SortNbrs(std::vector<uint32> myVector, int i)
 {
-		if (myVector.size() > 2)
-		{
-			std::vector<uint32> tmp;
-			for (s_t k = 0; k < myVector.size() / 2; k++)
-			{
-				tmp.push_back(myVector.at(k));
-				myVector.erase(myVector.begin() + k);
-			}
-			tmp = SortNbrs(tmp, ++i);
-			for (s_t k = tmp.size(); k > 0; k--)
-			{
-				myVector.push_back(tmp.at(k - 1));
-			}
-		}
-		if (myVector.size() == 1)
-		{
-			return myVector;
-		}
+	std::vector<uint32> A;
+	std::vector<uint32> B;
+	uint32	mid = 0;
+
+	// std::cout << T_BLUE << "\nIN: ";
+	// for (s_t i = 0; i < myVector.size(); i++)
+	// {
+	// 	std::cout << myVector.at(i) << " ";
+	// }
+	// std::cout << std::endl;
 
 
-		if (myVector.at(0) > myVector.at(1) && myVector.size() > 1)
-		{
-			uint32 tmp = myVector.at(0);
-			myVector.at(0) = myVector.at(1);
-			myVector.at(1) = tmp;
-		}
-		// if (myVector.size() > 2)
-		// {
-		// 	PrintContainers();
+	for (s_t i = 0; i < myVector.size(); i++)
+	{
+		mid += myVector.at(i);
+	}
 
-		// 	for (s_t k = 0; k < myVector.size(); k++)
-		// 	{
-		// 		for (s_t g = k + 1; g < myVector.size(); g++)
-		// 		{
-		// 			// std::cout << T_CYAN;
-		// 			// for (s_t i = 0; i < myVector.size(); i++)
-		// 			// {
-		// 			// 	std::cout << myVector.at(i) << " ";
-		// 			// }
-		// 			// std::cout << std::endl;
-					
-		// 			if (g == k)
-		// 			{
-		// 				continue;
-		// 			}
-		// 			if (myVector.at(k) > myVector.at(g))
-		// 			{
-		// 				// std::cout << "SWAP" << std::endl;
-		// 				uint32 tmp = myVector.at(k);
-		// 				myVector.at(k) = myVector.at(g);
-		// 				myVector.at(g) = tmp;
-						
-		// 				// std::cout << T_BLUE;
-		// 				// for (s_t i = 0; i < myVector.size(); i++)
-		// 				// {
-		// 				// 	std::cout << myVector.at(i) << " ";
-		// 				// }
-		// 				// std::cout << std::endl;
-		// 			}
-		// 		}
-		// 	}
-		// }
+	mid = mid / myVector.size();
+
+	for (s_t i = 0; i < myVector.size(); i++)
+	{
+		if (myVector.at(i) > mid)
+		{
+			A.push_back(myVector.at(i));
+		}
+		else
+		{
+			B.push_back(myVector.at(i));
+		}
+	}
+	// std::cout << "C" << std::endl;
+
+	if (A.size() > 2)
+	{
+		A = SortNbrs(A, ++i);
+
+		if (A.at(0) > A.at(1) && A.size() == 2)
+		{
+			uint32 tmp = A.at(0);
+			A.at(0) = A.at(1);
+			A.at(1) = tmp;
+		}
+	}
+	if (B.size() > 2)
+	{
+		B = SortNbrs(B, ++i);
+
+		if (B.at(0) > B.at(1) && B.size() == 2)
+		{
+			uint32 tmp = B.at(0);
+			B.at(0) = B.at(1);
+			B.at(1) = tmp;
+		}
+	}
+	// std::cout << "1" << std::endl;
+
+
+	// std::cout << T_GREEN << std::endl;
+	// for (s_t i = 0; i < A.size(); i++)
+	// {
+	// 	std::cout << A.at(i) << " ";
+	// }
+	// std::cout << std::endl;
+
+	// std::cout << T_MAGENTA << std::endl;
+	// for (s_t i = 0; i < B.size(); i++)
+	// {
+	// 	std::cout << B.at(i) << " ";
+	// }
+	// std::cout << std::endl;
+
+
+
+
+	if (A.at(0) < B.at(B.size() - 1))
+	{
+		for (s_t i = 0; i < B.size(); i++)
+		{
+			A.push_back(B.at(i));
+		}
+
+		return A;
+	}
+	else
+	{
+		for (s_t i = 0; i < A.size(); i++)
+		{
+			B.push_back(A.at(i));
+		}
+
+		return B;
+	}
+
 		return myVector;
 }
 
